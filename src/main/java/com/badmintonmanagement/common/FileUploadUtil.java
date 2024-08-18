3ce006e6e57764734c70544cd16e80c517e04cae
KhaiCX <101654542+KhaiCX@users.noreply.github.com>
2024-08-18 17:13:13 +0700

    KhaiCX: add FileUploadUtil common

---
+++ src/main/java/com/badmintonmanagement/common/FileUploadUtil.java
@@ -0,0 +1,56 @@
+package com.badmintonmanagement.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUploadUtil {
+	public static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+	public static void savedFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+		Path uploadPath = Paths.get(uploadDir);
+		if(!Files.exists(uploadPath)) {
+			Files.createDirectories(uploadPath);
+		}
+		try(InputStream inputStream = multipartFile.getInputStream()) {
+			Path filePath = uploadPath.resolve(fileName);
+			Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+			
+		} catch(IOException ex) {
+			throw new IOException("Could not save file: " + fileName, ex);
+		}
+	}
+	/**
+	 * Clear file image directory
+	 */
+	public static void cleanDir(String directory) {
+		Path dirPath = Paths.get(directory);
+		try {
+			Files.list(dirPath).forEach(file -> {
+				if(!Files.isDirectory(file)) {
+					try {
+						Files.delete(file);
+					} catch(IOException ex) {
+						LOGGER.error("Could not delete file: " + file);
+					}
+				}
+			});
+		} catch(IOException ex) {
+			LOGGER.error("Could not list directory: " + dirPath);
+		}
+	}
+	public static void removeDir(String dir) {
+		cleanDir(dir);
+		try {
+			Files.delete(Paths.get(dir));
+		} catch (IOException ex) {
+			LOGGER.error("Could not remove directory: " + dir);
+		}
+	}
+}
