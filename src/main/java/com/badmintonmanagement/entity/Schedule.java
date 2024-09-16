464badf8dd527854dab5b61a99deb6e4e6d86892
KhaiCX <101654542+KhaiCX@users.noreply.github.com>
2024-09-16 23:07:42 +0700

    KhaiCX: update structure schedule entity

--- src/main/java/com/badmintonmanagement/entity/Schedule.java
+++ src/main/java/com/badmintonmanagement/entity/Schedule.java
@@ -18,12 +18,18 @@ public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long scheduleId;
-    private String userOne;
-    private String userTwo;
+    private String user1;
+    private String user2;
     private LocalDateTime time;
-    private Integer resultUserOne;
-    private Integer resultUserTwo;
+    private Integer resultRound1User1;
+    private Integer resultRound2User1;
+    private Integer resultRound3User1;
+    private Integer resultRound1User2;
+    private Integer resultRound2User2;
+    private Integer resultRound3User2;
+    private Integer finalResultUser1;
+    private Integer finalResultUser2;
     private String category;
-    private String imageUserOne;
-    private String imageUserTwo;
+    private String imageUser1;
+    private String imageUser2;
 }
